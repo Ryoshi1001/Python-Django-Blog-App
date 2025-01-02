b25d300b763ff0933eaa203e0479497d43287164
ryoshi1001 <ryoshi16421@proton.me>
2025-01-02 02:44:55 -0800

    Post model created in model.py, Post model registered to Django Admin Panel, Django date Format string added, 3 Posts added using Django ORM

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-# unreal data for post 
-
-posts = [
-  {
-    'author': 'CoreyMS', 
-    'title': 'Blog Post 1', 
-    'content': "First post comment", 
-    'date_posted': 'August 14, 2024'
-  }, 
-  {
-    'author': 'Tomi Toko', 
-    'title': 'Blog Post 2', 
-    'content': "Second post comment", 
-    'date_posted': 'August 4, 2024'
-  }
-]
+from .models import Post
 
 # Create your views here.
 def home(request): 
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 
